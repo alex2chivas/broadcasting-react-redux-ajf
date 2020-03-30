dcda265a4c0d6c48837441be8f887b7d6aabf95f
Alexis Flores <alex2chivas@hotmail.com>
2020-03-30 00:40:16 -0700

    added userId that we get from gapi

--- src/component.js/reducers/authReducer.js
+++ src/component.js/reducers/authReducer.js
@@ -1,16 +1,19 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types'
 
 const INITIAL_STATE = {
-    isSignedIn: null
+    isSignedIn: null,
+    userId: null
+    // Note gapi.auth2.getAuthInstance().currentUser.get().getId()
+    // This get us an unique ID for the current person logged in
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case SIGN_IN:
-            return {...state, isSignedIn: true}
+            return {...state, isSignedIn: true, userId: action.payload}
 
         case SIGN_OUT:    
-            return {...state, isSignedIn: false}
+            return {...state, isSignedIn: false, userId: null}
         default:
             return state;
     }
