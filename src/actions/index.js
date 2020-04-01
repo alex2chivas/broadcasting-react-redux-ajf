68fe90378d371dcd32d7b25676677caaaddd450b
Alexis Flores <alex2chivas@hotmail.com>
2020-03-31 21:13:06 -0700

    added all the types since CRUD is being executed to help with dubugging

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,6 +16,7 @@ export const signOut = () => {
 
 export const createStream = formValues => async dispatch => {
 	const response = await streams.post('/streams', formValues);
+
 	dispatch({
 		type: CREATE_STREAM,
 		payload: response.data
