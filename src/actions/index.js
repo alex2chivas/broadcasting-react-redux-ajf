461d898606e19a48fb801448ed03debdd4d31e6a
Alexis Flores <alex2chivas@hotmail.com>
2020-04-01 22:29:48 -0700

    added inside our action creator for createStream, getState to get the userId from auth inside our store

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,13 +22,17 @@ export const signOut = () => {
 	};
 };
 
-export const createStream = formValues => async dispatch => {
-	const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+	const { userId } = getState().auth;
+	const response = await streams.post('/streams', { ...formValues, userId });
 
 	dispatch({
 		type: CREATE_STREAM,
 		payload: response.data
 	});
+	// Note
+	// Do some programmatic back to the root route
+	// Programtic navigation
 };
 
 export const fecthStreams = () => async dispatch => {
