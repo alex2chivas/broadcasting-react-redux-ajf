da416bcd2a1fbc13ec610ab27cd14daa7cf924f2
Alexis Flores <alex2chivas@hotmail.com>
2020-04-01 17:34:10 -0700

    added some notes regarding key interpolation

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,20 +1,28 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
-    isSignedIn: null,
-    userId: null
-    // Note gapi.auth2.getAuthInstance().currentUser.get().getId()
-    // This get us an unique ID for the current person logged in
-}
+	isSignedIn: null,
+	userId: null
+	// Note gapi.auth2.getAuthInstance().currentUser.get().getId()
+	// This get us an unique ID for the current person logged in
+};
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case SIGN_IN:
-            return {...state, isSignedIn: true, userId: action.payload}
+	switch (action.type) {
+		case SIGN_IN:
+			return { ...state, isSignedIn: true, userId: action.payload };
 
-        case SIGN_OUT:    
-            return {...state, isSignedIn: false, userId: null}
-        default:
-            return state;
-    }
-}
+		case SIGN_OUT:
+			return { ...state, isSignedIn: false, userId: null };
+		default:
+			return state;
+	}
+};
+
+// Note // ES6 syntax
+// const newState = { ...state }
+// newState[action.payload.id] = action.payload
+// return newState
+////////////////////////////////////////
+// Note // ES6 shorter syntax
+// return { ...state, [action.payload.id]: action.payload}
