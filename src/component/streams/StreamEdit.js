e889d16de7339048de3797851647d0c5eb661e97
Alexis Flores <alex2chivas@hotmail.com>
2020-04-02 21:43:23 -0700

    added history.push to navigate someone to the / url

--- src/component/streams/StreamEdit.js
+++ src/component/streams/StreamEdit.js
@@ -1,19 +1,35 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchStream } from '../../actions/index';
+import { fetchStream, editStream } from '../../actions/index';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends Component {
 	componentDidMount() {
 		this.props.fetchStream(this.props.match.params.id);
 	}
 
+	onSubmit = formValues => {
+		// console.log(this.props);
+		this.props.editStream(this.props.match.params.id, formValues);
+	};
+
 	render() {
 		if (!this.props.stream) {
 			return <div className='ui active centered inline loader' />;
 		}
 
-		return <div>{this.props.stream.title}</div>;
+		return (
+			<div>
+				<h3> Edit a Stream </h3>
+				<StreamForm
+					onSubmit={this.onSubmit}
+					// Note - lodash handles picking a certain key
+					initialValues={_.pick(this.props.stream, 'title', 'description')}
+				/>
+			</div>
+		);
 	}
 }
 
@@ -21,4 +37,4 @@ const mapStateToProps = (state, ownProps) => {
 	return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
